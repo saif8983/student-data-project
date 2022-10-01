6ae13221bb8bb80c6e7b19e0b5823e70f7fe2061
saif8983 <asaif8367@gmail.com>
2022-10-02 03:05:26 +0530

    formate app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter,Routes,Route} from "react-router-dom";
-import Navbar from './Components/Navbar'
-import Sidebar from './Components/Sidebar';
-import AddStudents from './Components/AddStudents';
-import ManageStudents from './Components/ManageStudents';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Sidebar from "./Components/Sidebar";
+import AddStudents from "./Components/AddStudents";
+import ManageStudents from "./Components/ManageStudents";
 function App() {
   return (
     <>
-    {/*That is layout of app i used react-router-dom*/}
-    <BrowserRouter>
-    <div class="container">
-  <div class="Navbar"><Navbar/></div>
-  <div class="sidebar"><Sidebar/></div>
-  <div class="main-container">
-  <Routes>
-  <Route exact path="/" element={<AddStudents/>}/>
-        <Route exact path="/Manage" element={<ManageStudents/>}/>
-  </Routes>
-  </div>
-</div>
-</BrowserRouter>
+      {/*That is layout of app i used react-router-dom*/}
+      <BrowserRouter>
+        <div class="container">
+          <div class="Navbar">
+            <Navbar />
+          </div>
+          <div class="sidebar">
+            <Sidebar />
+          </div>
+          <div class="main-container">
+            <Routes>
+              <Route exact path="/" element={<AddStudents />} />
+              <Route exact path="/Manage" element={<ManageStudents />} />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
