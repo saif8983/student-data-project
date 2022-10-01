79a95e8cd457fee62ba30473bb446884aa5a712e
saif8983 <asaif8367@gmail.com>
2022-10-02 03:21:49 +0530

    navbar completed

--- client/src/Utils/style.jsx
+++ client/src/Utils/style.jsx
@@ -1,95 +1,109 @@
 const style = {
+    //That is Navbar css
+    backgroundOfNavbar: {
+      backgroundColor: "white",
+      boxShadow: "none",
+      fontSize: "700px",
+    },
+    colorLogoNavBar: {
+      color: "rgba(88, 74, 74, 0.74)",
+      flexGrow: 1,
+      fontWeight: "700",
+    },
+    colorButtonNavbar: {
+      color: "black",
+      textTransform: "none",
+    },
+    //That is sidebar css
     SidebarButtonCss: {
-        color: 'rgba(0, 0, 0, 0.6)'
+      color: "rgba(0, 0, 0, 0.6)",
     },
     SidebarIconsCss: {
-        marginRight: '0.5rem'
+      marginRight: "0.5rem",
     },
+    //Addstudent and manageStudent css
     StudentsTitleCss: {
-        fontWeight: '600'
+      fontWeight: "600",
     },
     Timecss: {
-        fontWeight: '400'
+      fontWeight: "400",
     },
     FlexOfrow: {
-        display: 'flex',
-        direction: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center'
+      display: "flex",
+      direction: "row",
+      justifyContent: "space-around",
+      alignItems: "center",
     },
     TaxfieldSmallCss: {
-        width: '20rem',
-        height: '3rem'
+      width: "20rem",
+      height: "3rem",
     },
     FormControlCss: {
-        width: '20rem',
-        margin: 1
+      width: "20rem",
+      margin: 1,
     },
     TaxfieldLargeCss: {
-        width: '30rem',
-        height: '3rem'
+      width: "30rem",
+      height: "3rem",
     },
     FlexOfrowLargTaxField: {
-        display: 'flex',
-        direction: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        marginTop: '2rem'
+      display: "flex",
+      direction: "row",
+      justifyContent: "space-around",
+      alignItems: "center",
+      marginTop: "2rem",
     },
     AddStudentButtonCss: {
-        backgroundColor: '#FF2108',
-        width: '20rem',
-        marginTop: '1rem',
-        FontStyle: 'Bold',
-        fontWeight: '700'
+      backgroundColor: "#FF2108",
+      width: "20rem",
+      marginTop: "1rem",
+      FontStyle: "Bold",
+      fontWeight: "700",
     },
     flexOfrowSpaceBetween: {
-        display: 'flex',
-        direction: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-
-
+      display: "flex",
+      direction: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
     },
     TableHeadingCss: {
-        backgroundColor: ' #F33823',
-
-
+      backgroundColor: " #F33823",
     },
     TableHeadCellCss: {
-        color: '#FFFFFF',
-        FontStyle: 'SemiBold',
-        width: '25%'
+      color: "#FFFFFF",
+      FontStyle: "SemiBold",
+      width: "25%",
     },
     TabelRowCellCss: {
-        width: '20%',
-        boxShadow: 'none'
+      width: "20%",
+      boxShadow: "none",
     },
     TableIconsCss: {
-        marginRight: '1rem'
+      marginRight: "1rem",
     },
     TableImgCss: {
-        width: '100%',
-        height: '100%',
-        marginLeft: "-2rem"
+      width: "100%",
+      height: "100%",
+      marginLeft: "-2rem",
     },
     ModalBox: {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 400,
-        backgroundColor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        padding: 4,
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      width: 400,
+      backgroundColor: "background.paper",
+      border: "2px solid #000",
+      boxShadow: 24,
+      padding: 4,
     },
     FlexofrowModa: {
-        display: 'flex',
-        direction: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        marginTop: '2rem'
-    }
-}
-export default style
+      display: "flex",
+      direction: "row",
+      justifyContent: "space-around",
+      alignItems: "center",
+      marginTop: "2rem",
+    },
+  };
+  export default style;
+  
