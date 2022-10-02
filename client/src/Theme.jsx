169f167e946340718dab2fe12386e5f2d97ac9cf
saif8983 <asaif8367@gmail.com>
2022-10-02 10:57:34 +0530

    font

--- client/src/Theme.jsx
+++ client/src/Theme.jsx
@@ -1,5 +1,6 @@
 
 import { createTheme} from '@mui/material/styles';
+{/*font of the app*/}
 const themeOfLogo = createTheme({
     typography: {
       fontFamily: ['IBM Plex Sans, sans-serif'].join(','),
